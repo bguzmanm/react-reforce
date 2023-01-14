845a6e01b85377566550aa7c26392025ba042ee4
Brian Guzmán <b.guzman.m@me.com>
2023-01-14 14:27:30 -0300

    agrego paypal

--- src/components/home.js
+++ src/components/home.js
@@ -1,41 +1,45 @@
 import React from "react";
-import { collection, getDocs } from "firebase/firestore";
+// import { collection, getDocs } from "firebase/firestore";
 
-import { db } from './firebase';
-import { ListMessage } from "./listMessage";
-import { OneMessage } from "./oneMessage";
+// import { db } from './firebase';
+// import { ListMessage } from "./listMessage";
+// import { OneMessage } from "./oneMessage";
 
 function Home() {
   const [messages, setMessages] = React.useState([]);
 
-  React.useEffect(() => {
-    try {
-      const readData = async (coleccion) => {
-        const data = await getDocs(collection(db, coleccion));
-        const arrDocs = data.docs.map(doc => (doc.data()));
-        setMessages(arrDocs);
-      }
+  setMessages([{ user: "brian", message_text: "hola" }]);
 
-      readData('messages');
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   try {
+  //     const readData = async (coleccion) => {
+  //       const data = await getDocs(collection(db, coleccion));
+  //       const arrDocs = data.docs.map(doc => (doc.data()));
+  //       setMessages(arrDocs);
+  //     }
+
+  //     readData('messages');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
 
 
 
   return (
-    <ListMessage>
-      {
-        messages.map(mensaje => (
-          <div>
-            <OneMessage
-              user={mensaje.user}
-              message_text={mensaje.message_text} />
-          </div>
-        ))
-      }
-    </ListMessage>
+    // <ListMessage>
+    //   {
+    //     messages.map(mensaje => (
+    //       <div>
+    //         <OneMessage
+    //           key={mensaje.message_text}
+    //           user={mensaje.user}
+    //           message_text={mensaje.message_text} />
+    //       </div>
+    //     ))
+    //   }
+    // </ListMessage>
+    <p>Hola</p>
   )
 }
 
